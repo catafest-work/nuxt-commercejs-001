749aa6d018ec049bea193af6f0ebc396dd18cbe5
catalin.festila <catafest76@gmail.com>
2022-05-16 16:35:20 +0300

    first commit - show merchant, categories and products

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <pre>{{ JSON.stringify(merchant, null, 2) }}</pre>
+    <pre>{{ JSON.stringify(categories, null, 2) }}</pre>
+    <pre>{{ JSON.stringify(products, null, 2) }} </pre>  
+  </div>
+</template>
+
+<script>
+  import commerce from "~/common/commerce"
+
+  export default {
+    async asyncData() {
+      const merchant = await commerce.merchants.about()
+      const {data: categories} = await commerce.categories.list()
+      const {data: products} = await commerce.products.list()
+      return {merchant, categories,products,};
+    },
+  };
+</script>
